d7d621bf13d15f6dbea0e2110b69310e511cd57d
dukechain2333 <qzh13918925958@outlook.com>
2020-07-28 21:28:10 +0800

    添加了kmeans_cluster()方法，实现了对经过预处理后数据的聚类

--- auto_test.py
+++ auto_test.py
@@ -1,6 +1,13 @@
 #  @author Duke Chain
 #  @File:auto_test.py
 #  @createTime 2020/07/26 22:32:26
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from classify import *
 
+x, y = make_blobs(n_samples=500,
+                  n_features=2,
+                  centers=4,
+                  random_state=1)
 
-
+kmeans_cluster(3, x)
--- classify.py
+++ classify.py
@@ -3,6 +3,8 @@
 #  @createTime 2020/07/27 20:34:27
 import numpy as np
 from weight import AHPgetWeight
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 
 def preprocessing(timeseries, weightmatrix):
@@ -21,7 +23,7 @@ def preprocessing(timeseries, weightmatrix):
 
     # 对时间序列进行降维（时间序列和权重矩阵点乘）
     series = np.dot(timeseries, weightmatrix)
-    print("降维后的时间序列为：", series)
+    print("降维后的时间序列为：\n", series)
     print("------------------------------")
 
     # 对数据进行预处理及重构
@@ -36,10 +38,40 @@ def preprocessing(timeseries, weightmatrix):
     print("完成预处理流程后的数据为：\n", collections)
     print("------------------------------")
 
+    return collections
 
-weightMatrix = AHPgetWeight(5, [0.5, 4, 3, 3, 7, 5, 5, 0.5, 1 / 3, 1])
-timeSeries = [[1, 32, 3, 14, 45],
-              [6, 47, 18, 19, 10],
-              [11, 2, 13, 14, 65],
-              [16, 7, 18, 9, 20]]
-preprocessing(timeSeries, weightMatrix)
+
+def kmeans_cluster(cluster, collections):
+    """
+    对处理后的序列聚类
+
+    对通过preprocessing()方法处理后的时间序列进行k-means聚类
+
+    Args:
+        cluster(int):传入聚类簇的数量
+        collections(m*2的矩阵)：传入一个经过preprocessing()处理的m*2矩阵
+
+    Returns:
+
+    """
+
+    # 创建聚类器并聚类
+    estimator = KMeans(n_clusters=cluster)
+    result = estimator.fit(collections)
+
+    # 可视化聚类结果
+    predict_labels = result.labels_
+    plt.scatter(collections[:, 0], collections[:, 1], c=predict_labels)
+    plt.xlabel("Index")
+    plt.ylabel("Latitude")
+    plt.title("The result of clustering")
+    plt.show()
+
+
+# weightMatrix = AHPgetWeight(5, [0.5, 4, 3, 3, 7, 5, 5, 0.5, 1 / 3, 1])
+# timeSeries = [[1, 32, 3, 14, 45],
+#               [6, 47, 18, 19, 10],
+#               [11, 2, 13, 14, 65],
+#               [16, 7, 18, 9, 20]]
+# data = preprocessing(timeSeries, weightMatrix)
+# kmeans_cluster(1, data)
