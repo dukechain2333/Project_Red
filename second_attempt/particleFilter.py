744eb772acada6d7b667fa962115bed99302b46d
dukechain2333 <qzh13918925958@outlook.com>
2020-08-10 22:45:54 +0800

    给weight_calculate()方法增加了权重归一化

--- second_attempt/particleFilter.py
+++ second_attempt/particleFilter.py
@@ -11,14 +11,14 @@ def weight_calculate(particleMatrix, originMatrix):
     """
     权重计算
 
-    假设矩阵按照正态分布，则采用概率密度函数（P.D.F）求出点的权重
+    假设矩阵按照正态分布，则采用概率密度函数（P.D.F）求出点的权重，并进行归一化
 
     Args:
         particleMatrix:传入按照正态分布的随机粒子矩阵
         originMatrix:传入需要计算的矩阵
 
     Returns：
-        weightMatrix:权重矩重
+        weightMatrix:归一化后的权重矩阵
     """
 
     # 求出期望值
@@ -29,12 +29,22 @@ def weight_calculate(particleMatrix, originMatrix):
     rowNum = particleMatrix.shape[0]
 
     # 初始化权重矩阵
+    weightMatrixtem = np.zeros(particleMatrix.shape)
     weightMatrix = np.zeros(particleMatrix.shape)
 
     # 使用概率密度函数求出权重矩阵
     for row in range(rowNum):
         for col in range(colNum):
-            weightMatrix[row, col] = (1 / sqrt(2 * pi)) * exp(
+            weightMatrixtem[row, col] = (1 / sqrt(2 * pi)) * exp(
                 (particleMatrix[row, col] - expectedMatrix[0, col] ** 2) / (-2))
+
+    # 归一化权重矩阵
+    weightSum = np.zeros([1, colNum])
+
+    for i in range(rowNum):
+        weightSum += weightMatrixtem[i]
+
+    for row in range(rowNum):
+        weightMatrix[row] = weightMatrixtem[row] / weightSum
 
     return weightMatrix
